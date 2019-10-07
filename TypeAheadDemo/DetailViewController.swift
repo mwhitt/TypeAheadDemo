698f83e2d27a66df24f60cbc9ffba48eb27d7b5a
Matthew Whittaker <mwhitt89@gmail.com>
2019-10-07 16:08:26 -0700

    First commit

---
+++ TypeAheadDemo/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  TypeAheadDemo
+//
+//  Created by Matthew Whittaker on 10/7/19.
+//  Copyright © 2019 One Stream Ventures. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
